e315435452a201ce4c2369ef770f992f1e273fb7
CS360 Team 2 <elliae01@students.ipfw.edu>
2017-10-27 18:26:54 -0400

    Add files via upload

---
+++ helloA.py
@@ -0,0 +1,23 @@
+import time
+import threading
+import helloClient
+
+
+#This serves as a test function to run on the helloClient
+#Will utlize connection2 and transmit via TCP
+def hello1():
+    message ='The sky is the limit'
+    helloClient.connection(message)
+    message2 = 'The sky is the limit'
+    repeat = 8
+
+    while repeat >0:
+        time.sleep(2)
+        helloClient.connection(message2)
+        repeat-=1
+
+if __name__=='__main__':
+    hello1()
+
+
+        
---
+++ helloClient.py
@@ -0,0 +1,51 @@
+import socket
+import sys
+import threading
+import myo_raw
+import helloA
+
+
+
+def connection(message):
+        
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect the socket to the port where the server is listening
+        server_address = ('10.0.0.170', 10000)
+        print (sys.stderr, 'connecting to %s port %s' % server_address)
+        sock.connect(server_address)
+
+
+
+        try:
+            
+            # Encode data
+            message_as_bytes = str.encode(message)
+            type(message_as_bytes) # ensure it is byte representation
+            
+            #Decode data, not needed for now
+            my_decoded_str = message_as_bytes.decode()
+            type(my_decoded_str)
+            
+            #Print and send all data
+            print (sys.stderr, 'transmitting "%s"' % message)
+            sock.sendall(message_as_bytes)
+
+            # Look for server response
+            data_received = 0
+            data_expected = len(message)
+            
+            while data_received < data_expected:
+                data = sock.recv(16)
+                data_received += len(data)
+                print (sys.stderr, 'received "%s"' % data)
+
+        finally:
+            print (sys.stderr, 'closing socket')
+            sock.close()
+
+
+
+
+
