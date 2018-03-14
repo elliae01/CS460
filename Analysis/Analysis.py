8ac853125819d39ba6cb9a1300ac637d895840a0
unlimiteddigits <ericmw02@students.ipfw.edu>
2018-03-13 23:32:09 -0400

    Analysis of total distance stored in database

---
+++ Analysis/Analysis.py
@@ -0,0 +1,79 @@
+from multiprocessing import Process, Manager
+from multiprocessing.managers import *
+import pandas as pd
+from pandas import DataFrame
+import cx_Oracle
+import os
+import math
+from Server.UserInformation import *
+from Reactionalytics import *
+
+def __init_():
+    print("hello from Analysis init")
+
+#getDataFrame from Database
+def getDataFrame():
+    ip = 'localhost'
+    port = 1521
+    SID = 'orcl'
+    dsn_tns = cx_Oracle.makedsn(ip, port, SID)
+    #print(dsn_tns)
+    conn_str = u'SYSMAN/System_Admin1@localhost:1521/orcl'
+    database = cx_Oracle.connect('SYSMAN', 'System_Admin1', dsn_tns)
+    cursor = database.cursor()
+
+    sSQLCMD = 'Select s.S_INDEX,s.S_DATE,s.S_SHOOTER.Id,s.S_SHOOTER.Loc.x,s.S_SHOOTER.Loc.y,s.S_SHOOTER.Loc.z,'
+    sSQLCMD = sSQLCMD +'s.S_SHOOTER.hostility,s.S_SHOOTER.hit,s.S_SHOOTER.hr,s.S_SHOOTER.arm.emg.emg0,' \
+                       's.S_SHOOTER.arm.emg.emg1,s.S_SHOOTER.arm.emg.emg2,' \
+                       's.S_SHOOTER.arm.emg.emg3,s.S_SHOOTER.arm.emg.emg4,s.S_SHOOTER.arm.emg.emg5,' \
+                       's.S_SHOOTER.arm.emg.emg6,s.S_SHOOTER.arm.emg.emg7,s.S_SHOOTER.arm.roll,' \
+                       's.S_SHOOTER.arm.pitch,s.S_SHOOTER.arm.heading,s.S_SHOOTER.shot,s.S_SHOOTER.body.heading,' \
+                       's.S_SHOOTER.body.roll,s.S_SHOOTER.body.pitch,s.S_SHOOTER.head.heading,s.S_SHOOTER.head.roll,' \
+                       's.S_SHOOTER.head.pitch FROM Shooter_Table s'
+    cursor.execute(sSQLCMD)
+    df = DataFrame(cursor.fetchall())
+    df.rename(columns={0:"INDEX", 1: "Date", 2: "Id", 3: "Loc.x", 4: "Loc.y", 5: "Loc.z", 6: "Hostility", 7: "Hit", 8: "Heart Rate", 9: "Emg0", 10: "Emg1", 11: "Emg2", 12: "Emg3", 13: "Emg4",14: "Emg5",15: "Emg6",16: "Emg7",17: "Arm.Roll",18: "Arm.Pitch",19: "Arm.Heading",20: "Shot",21: "Hody.Heading", 22: "Body.Roll", 23: "Body.Pitch",24: "Head.Heading",25: "Head.Roll", 26: "Head.Pitch"}, inplace=True)
+    database.close
+    return df
+
+def pExportToCSV(df,sFileName):
+    dir_path = os.path.dirname(os.path.realpath("__file__"))
+    #print (dir_path)
+    dir_path = dir_path+"\\"+sFileName+".csv"
+    #print(df.head())
+    try:
+        df.to_csv(dir_path)
+        print("Saved " + sFileName)
+    except:
+        print("Could not save " + sFileName)
+        pass
+    return 0
+
+# Distance function
+def distance2D(xii,xi,yii,yi):
+    sq1 = (xi-xii)*(xi-xii)
+    sq2 = (yi-yii)*(yi-yii)
+    return math.sqrt(sq1 + sq2)
+
+if __name__ == '__main__':
+    df=getDataFrame()
+    #print(df.head())
+    pExportToCSV(df,"DataBeforeSort")
+    dfSortedByIndex=df.sort_values('INDEX', ascending=True)
+    pExportToCSV(dfSortedByIndex,"DataAfterSort")
+    dfSortedByDate=df.sort_values('Date', ascending=True)
+    pExportToCSV(dfSortedByIndex,"DataAfterSortDate")
+
+    r=Reactionalytics(dfSortedByDate)
+    count=r.rowcount()
+    d=0.0
+    for i in range(count-1):
+        x1 = r.getX(i)
+        y1 = r.getY(i)
+        x2 = r.getX(i+1)
+        y2 = r.getY(i+1)
+        d2 = distance2D(x1,x2,y1,y2)
+        d = d + d2
+        #print(d2,r.distanceFromLastPoint(i+1))
+        #print(d2,r.getINDEX(i))
+    print("Total Distance = ", d)
---
+++ Analysis/Reactionalytics.py
@@ -0,0 +1,145 @@
+from multiprocessing import Process, Manager
+from multiprocessing.managers import *
+import pandas as pd
+from pandas import DataFrame
+import cx_Oracle
+import os
+import math
+from Server.UserInformation import *
+
+print("Hello, Welcome to Reactionalytics.py")
+
+class Reactionalytics:
+    df = None
+
+    def __init__(self,MyDataFrame):
+        self.cCol4INDEX = 0
+        self.cCol4Date = 1
+        self.cCol4Id = 2
+        self.cCol4LocX = 3
+        self.cCol4LocY = 4
+        self.cCol4LocZ = 5
+        self.cCol4Hostility = 6
+        self.cCol4Hit = 7
+        self.cCol4HeartRate = 8
+        self.cCol4Emg0 = 9
+        self.cCol4Emg1 = 10
+        self.cCol4Emg2 = 11
+        self.cCol4Emg3 = 12
+        self.cCol4Emg4 = 13
+        self.cCol4Emg5 = 14
+        self.cCol4Emg6 = 15
+        self.cCol4Emg7 = 16
+        self.cCol4ArmRoll = 17
+        self.cCol4ArmPitch = 18
+        self.cCol4ArmHeading = 19
+        self.cCol4Shot = 20
+        self.cCol4BodyHeading = 21
+        self.cCol4BodyRoll = 22
+        self.cCol4BodyPitch = 23
+        self.cCol4HeadHeading = 24
+        self.cCol4HeadRoll = 25
+        self.cCol4HeadPitch = 26
+        self.df = MyDataFrame
+
+    def getDate(self, row):
+        return self.df.iat[row, self.cCol4Date]
+
+    def getINDEX(self, row):
+        return self.df.iat[row, self.cCol4INDEX]
+
+    def getID(self, row):
+        return self.df.iat[row, self.cCol4Id]
+
+    def getX(self, row):
+        #print("Calculating X for index "+str(self.df.iat[row, self.cCol4INDEX]))
+        return self.df.iat[row, self.cCol4LocX]
+
+    def getY(self, row):
+        return self.df.iat[row, self.cCol4LocY]
+
+    def getZ(self, row):
+        return self.df.iat[row, self.cCol4LocZ]
+
+    def getHostility(self, row):
+        return self.df.iat[row, self.cCol4Hostility]
+
+    def getHit(self, row):
+        return self.df.iat[row, self.cCol4Hit]
+
+    def getHeartRate(self, row):
+        return self.df.iat[row, self.cCol4HeartRate]
+
+    def getEmg0(self, row):
+        return self.df.iat[row, self.cCol4Emg0]
+
+    def getEmg1(self, row):
+        return self.df.iat[row, self.cCol4Emg1]
+
+    def getEmg2(self, row):
+        return self.df.iat[row, self.cCol4Emg2]
+
+    def getEmg3(self, row):
+        return self.df.iat[row, self.cCol4Emg3]
+
+    def getEmg4(self, row):
+        return self.df.iat[row, self.cCol4Emg4]
+
+    def getEmg5(self, row):
+        return self.df.iat[row, self.cCol4Emg5]
+
+    def getEmg6(self, row):
+        return self.df.iat[row, self.cCol4Emg6]
+
+    def getEmg7(self, row):
+        return self.df.iat[row, self.cCol4Emg7]
+
+    def getArmRoll(self, row):
+        return self.df.iat[row, self.cCol4ArmRoll]
+
+    def getArmPitch(self, row):
+        return self.df.iat[row, self.cCol4ArmPitch]
+
+    def getArmHeading(self, row):
+        return self.df.iat[row, self.cCol4ArmHeading]
+
+    def getShot(self, row):
+        return self.df.iat[row, self.cCol4Shot]
+
+    def getBodyHeading(self, row):
+        return self.df.iat[row, self.cCol4BodyHeading]
+
+    def getBodyRoll(self, row):
+        return self.df.iat[row, self.cCol4BodyRoll]
+
+    def getBodyPitch(self, row):
+        return self.df.iat[row, self.cCol4BodyPitch]
+
+    def getHeadHeading(self, row):
+        return self.df.iat[row, self.cCol4HeadHeading]
+
+    def getHeadRoll(self, row):
+        return self.df.iat[row, self.cCol4HeadRoll]
+
+    def getHeadPitch(self, row):
+        return self.df.iat[row, self.cCol4HeadPitch]
+
+    def rowcount(self):
+        return self.df['INDEX'].count()
+
+    def columncount(self):
+        return self.df.shape[1]
+
+    def distanceFromLastPoint(self,row):
+        d=0
+        if row>0:
+            xi = self.getX(row-1)
+            xii = self.getX(row)
+            yi = self.getY(row-1)
+            yii = self.getY(row)
+            sq1 = (xii-xi)*(xii-xi)
+            sq2 = (yii-yi)*(yii-yi)
+            d = math.sqrt(sq1 + sq2)
+        return d
+
+
